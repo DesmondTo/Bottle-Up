9c3240df1d47f51a076a177e846e81c533c26496
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2023-01-14 23:18:50 +0800

    Update latest chat once received

--- frontend/src/component/chat/messageText/index.js
+++ frontend/src/component/chat/messageText/index.js
@@ -2,11 +2,23 @@ import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import styles from './styles';
 import { getLatestChatAPI } from '../../../api/chat-api';
+import io from 'socket.io-client';
+import {
+  URL_CHAT_SVC_SOCKET,
+  PATH_CHAT_SVC_SOCKET,
+} from '../../../api/constants';
+
+const chat_socket = io(URL_CHAT_SVC_SOCKET, {
+  path: PATH_CHAT_SVC_SOCKET,
+  withCredentials: true,
+});
 
 function MessageText({ bottleID }) {
   const [latestChat, setLatestChat] = useState('');
 
   useEffect(() => {
+    chat_socket.emit('join-bottle', bottleID);
+
     const getLatestChat = async bottleID => {
       const latestChatResponse = await getLatestChatAPI(bottleID);
       const latestChatSuccess = latestChatResponse.success;
@@ -21,10 +33,24 @@ function MessageText({ bottleID }) {
     };
 
     getLatestChat(bottleID);
+
+    chat_socket.on(
+      'receive-chat',
+      (isSentSuccess, feedback, sender, chatSent) => {
+        isSentSuccess && getLatestChat(bottleID);
+      },
+    );
+
+    return () => {
+      chat_socket.off('join-bottle');
+      chat_socket.off('receive-chat');
+    };
   }, []);
   return (
     <View>
       <Text style={{ ...styles.messageText }}>{latestChat.text}</Text>
+      <Text>{latestChat.username}</Text>
+      <Text>{latestChat.createdAt}</Text>
     </View>
   );
 }

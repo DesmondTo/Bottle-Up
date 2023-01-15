6a15c2b23683e73e0afbdffcab9f287bc4b72f23
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2023-01-15 09:14:08 +0800

    Change tab icons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { ImageBackground, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, Text } from 'react-native';
 
 import UserContext from './context/user-context';
 import {
@@ -25,25 +25,48 @@ const Tab = createBottomTabNavigator();
 const Bottle = () => {
   const userCtx = useContext(UserContext);
   const isLoggedIn = userCtx.isSignedIn;
-  const image = require("./asset/background.png");
+  const image = require('./asset/background.png');
 
   return (
     <ImageBackground source={image} resizeMode="cover" style={styles.image}>
       <NavigationContainer theme={defaultTheme}>
-          <Tab.Navigator screenOptions={{ headerShown: false }}>
-            {isLoggedIn ? (
-              <Tab.Group>
-                <Tab.Screen name="Ocean" component={OceanStackScreen} />
-                <Tab.Screen name="Chat" component={ChatStackScreen} />
-                <Tab.Screen name="Profile" component={ProfileStackScreen} />
-              </Tab.Group>
-            ) : (
-              <Tab.Group>
-                <Tab.Screen name="SignIn" component={SignInStackScreen} />
-                <Tab.Screen name="SignUp" component={SignUpStackScreen} />
-              </Tab.Group>
-            )}
-          </Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => {
+            return {
+              headerShown: false,
+              tabBarIcon: ({ focused, color, size }) => {
+                let textIcon;
+
+                if (route.name === 'Ocean') {
+                  textIcon = '🌊';
+                } else if (route.name === 'Chat') {
+                  textIcon = '💬';
+                } else if (route.name === 'Profile') {
+                  textIcon = '⚙️';
+                } else if (route.name === 'SignIn') {
+                  textIcon = '⎆';
+                } else if (route.name === 'SignUp') {
+                  textIcon = '🙋‍♂️';
+                }
+
+                // You can return any component that you like here!
+                return <Text>{textIcon}</Text>;
+              },
+            };
+          }}>
+          {isLoggedIn ? (
+            <Tab.Group>
+              <Tab.Screen name="Ocean" component={OceanStackScreen} />
+              <Tab.Screen name="Chat" component={ChatStackScreen} />
+              <Tab.Screen name="Profile" component={ProfileStackScreen} />
+            </Tab.Group>
+          ) : (
+            <Tab.Group>
+              <Tab.Screen name="SignIn" component={SignInStackScreen} />
+              <Tab.Screen name="SignUp" component={SignUpStackScreen} />
+            </Tab.Group>
+          )}
+        </Tab.Navigator>
       </NavigationContainer>
     </ImageBackground>
   );
@@ -55,9 +78,8 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    justifyContent: "center"
-  }
+    justifyContent: 'center',
+  },
 });
-
 
 export default Bottle;

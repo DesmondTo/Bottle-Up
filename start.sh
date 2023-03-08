fcc51c22ba82b87911bfd6a1ee57d08eef363045
Desmond To <e0550619@u.nus.edu>
2023-03-08 14:45:10 +0800

    Update script

--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Run 'npx react-native start' and 'npx react-native run-ios' commands in 'frontend' folder
-cd frontend
-npx react-native start &
-npx react-native run-ios &
-
 # Run 'npm run dev' command in other folders
-cd ../user-service
-npm run dev &
+cd user-service
+npm start &
 
 cd ../bottle-service
-npm run dev &
+npm start &
 
 cd ../chat-service
-npm run dev &
+npm start &
+
+# Run 'npx react-native start' and 'npx react-native run-ios' commands in 'frontend' folder
+cd ../frontend
+npx react-native start &
+npx react-native run-ios --simulator='iPhone 14s' &
+npx react-native run-ios --simulator='iPhone 14 Pro (16.0)'
